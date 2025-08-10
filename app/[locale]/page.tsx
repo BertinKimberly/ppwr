519d5ec44f302323bd8c692d340ca3877b63dc05
Bertin <iradukundabertin082@gmail.com>
2025-08-11 01:10:43 +0200

    add landing

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-    </div>
-  )
-}
+   return (
+      <div className="w-full min-h-screen flex items-center justify-center gap-4 flex-col">
+         <h1>Welcome to the Home Page</h1>
+         <Link
+            href="/sign-in"
+            className="text-primary hover:underline"
+         >
+            Get Started
+         </Link>
+      </div>
+   );
+};
 
-export default Home
+export default Home;
